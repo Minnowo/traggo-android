c92a762392b633aaaf4270488471f32eaa076d6c
Alice Nyaa <62451415+Minnowo@users.noreply.github.com>
2024-10-20 17:25:17 -0400

    Actions test (#2)
    * Actions
    
    * Make build action
    
    Update build.yml
    
    Update build.yml

--- build.sh
+++ build.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
 # This is for use by the Docker builder
-# docker run --rm -it -v .:/mnt traggo-android-builder sh build.sh
 
 # Build Traggo
 make -C ./traggo
 
+
+# I couldn't figure out how to mount the keystore file so we're doing this
+if [ -n "$KEYSTORE_JKS_BASE64" ]; then
+
+    echo "$KEYSTORE_JKS_BASE64" | base64 -d > "$RELEASE_STORE_FILE"
+fi
+
+
 # Build the app
-./gradlew build
+./gradlew --no-daemon build
+
+
+if [ -e "$RELEASE_STORE_FILE" ]; then
+
+    echo "Release store file exists: $RELEASE_STORE_FILE"
+    rm -f "$RELEASE_STORE_FILE"
+fi
 
