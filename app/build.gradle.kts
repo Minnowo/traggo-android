90fba83063832ff1fb4902748baac2bf22ea0b77
Minnowo <62451415+Minnowo@users.noreply.github.com>
2025-06-14 16:22:28 -0400

    update traggo version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.traggo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
