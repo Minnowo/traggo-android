14b14c86ac3e654f0f360362a052960c6533b7eb
Minnowo <62451415+Minnowo@users.noreply.github.com>
2026-03-04 12:37:35 -0500

    update the app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.minnowo.traggo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.1.0"
+        versionCode = 7
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
